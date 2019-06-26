c836cb999c12b4a1c68158cab774a31598ad8678
Seun-O <12373359+Seun-O@users.noreply.github.com>
2019-06-25 23:11:29 -0400

    Testing controlls

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,19 @@
 // Test away!
+import React from "react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
+import "jest-dom/extend-expect";
+
+import Controls from "./Controls";
+
+//Cleans up after each test case
+afterEach(() => cleanup());
+
+//Test to see if the component renders.
+it("renders without crashing ", () => {
+  render(<Controls />);
+});
+
+it("should have a button", () => {
+  const { getByText } = render(<Controls />);
+  const close_gate = getByText(/close gate/i || /open gate/i);
+});
